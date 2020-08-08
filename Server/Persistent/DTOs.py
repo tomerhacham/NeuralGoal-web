c7e22830070d763aa0d1841480562811c16b7361
Tomer Hacham <hachamto@post.bgu.ac.il>
2020-08-09 00:29:03 +0300

    add dataclass as DTOs

--- Server/Persistent/DTOs.py
+++ Server/Persistent/DTOs.py
@@ -1,1 +1,35 @@
-#todo:implement
+import datetime
+from dataclasses import dataclass
+# todo:implement
+# implement as datatclass: require python version 3.7 or above
+
+@dataclass
+class upcoming_match:
+    league:str
+    date:datetime.datetime
+    round=int
+    home_team_name=str
+    away_team_name=str
+    home_team_rank=int
+    away_team_rank=int
+    home_team_scored=float
+    away_team_scored=float
+    home_team_received=float
+    away_team_received=float
+    home_att=int
+    away_att=int
+    home_def=int
+    away_def=int
+    home_mid=int
+    away_mid=int
+    home_odds_n=float
+    draw_odds_n=float
+    away_odds_n=float
+    home_odds_nn=float
+    draw_odds_nn=float
+    away_odds_nn=float
+
+@dataclass
+class match(upcoming_match):
+    result:str
+
