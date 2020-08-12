0815841bcd42316c1bb25c11a1d45efde99d47b7
Tomer Hacham <hachamto@post.bgu.ac.il>
2020-08-13 01:38:39 +0300

    need to implement unpacking

--- Server/Persistent/DBController.py
+++ Server/Persistent/DBController.py
@@ -1,7 +1,14 @@
+from Server.Persistent.DTOs import match
 from Server.Persistent.MongoDBConnection import MongoDBConnection
 
 DBConnection = MongoDBConnection()
-
+ListOfKeys=['Game Date','Home Team','Away Team','Home ATT','Away ATT',
+            'Home DEF','Away DEF','Home MID','Away MID','Home Win Odds','Draw Odds',
+            'Away Win Odds','Winner','Home win Odds not normal','Draw Odds not normal',
+            'Away win Odds not normal','Home Team Rank','Away Team Rank','Home Team Scored Goals',
+            'Home Team Received Goals','Away Team Scored Goals','Away Team received Goals']
+#TODO: inform Andrey missing 'League' attribute on the DB
+#TODO: inform Andrey missing the dict should be in the same order and the same names of the keys as the dataclass
 
 def getAllData(as_dataframe=False):
     #TODO:implement
@@ -9,7 +16,8 @@ def getAllData(as_dataframe=False):
     @param as_dataframe: boolean indicate if to return the data as Pandas DataFrame or list of DTOs
     @return: all the match in match table
     '''
-    return DBConnection.DevDBGames.find()
+    x= DBConnection.DevDBGames.find()
+    return x
     #return mongoConnection.ProductionDBMainTable.find()
 
 def getUpcomingGames(league,as_dataframe=False):
@@ -51,3 +59,5 @@ allData = getAllData()
 
 for game in allData:
   print(game)
+  sliced = {k:game[k] for k in ListOfKeys if k in game}
+  print(match(**sliced))
