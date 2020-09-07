0476d57bcf903e3b1ae5f3fc262a747545d67587
Tomer Hacham <hachamto@post.bgu.ac.il>
2020-09-07 14:02:29 +0300

    remove gRPC generated files

--- Server/protocs/proto command.bat
+++ Server/protocs/proto command.bat
@@ -1,3 +1,3 @@
 ECHO "Compailing"
-python -m grpc_tools.protoc -I="C:\Users\Tomer Hacham\Desktop\gRPC\protocs" --python_out=. --grpc_python_out=. "C:\Users\Tomer Hacham\Desktop\gRPC\protocs\match.proto"
+python -m grpc_tools.protoc -I="C:\Project\NeuralGoal-web\Server\protocs" --python_out=. --grpc_python_out=. "C:\Project\NeuralGoal-web\Server\protocs\match.proto"
 PAUSE
