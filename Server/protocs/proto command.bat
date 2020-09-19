d4c8c7a212d14f2e64c1c689aa5c273db6c793b2
Tomer Hacham <hachamto@post.bgu.ac.il>
2020-09-19 15:11:15 +0300

    update protoc file

--- Server/protocs/proto command.bat
+++
@@ -1,3 +0,0 @@
-ECHO "Compailing"
-python -m grpc_tools.protoc -I="C:\Project\NeuralGoal-web\Server\protocs" --python_out=. --grpc_python_out=. "C:\Project\NeuralGoal-web\Server\protocs\match.proto"
-PAUSE
